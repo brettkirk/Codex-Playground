413d4f0fcff25ad87dd17952cbf14851a2d11126
Brett Kirk <brett_kirk@me.com>
2025-10-29 23:37:52 -0700

    Add themed 404 page and reroute placeholder links

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import NotFound from './NotFound.jsx'
+
+const normalizedPath = window.location.pathname.replace(/\/+$/, '') || '/'
+const showNotFound = normalizedPath !== '/' && normalizedPath !== '/index.html'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {showNotFound ? <NotFound /> : <App />}
   </StrictMode>,
 )
